4ec8087f2039b293dc2e9452f41a23be398b6f59
Matthew Tutak <mtutak91@gmail.com>
2016-11-10 17:51:29 -0600

    updating full js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,89 +1,109 @@
 
 // VARIABLES ======================
-var animalArray = ["border collie", "red panda", "elephant", "sugar glider"]
+var animalArray = ["border collie", "red panda", "elephant", "sugar glider"];
 
 var click = 0;
 
 var total = animalArray.length;
 	
 
-	// 	var state = $(this).attr
-	// 	//replace? _s, ''
-	// });
+//create initial buttons from array================================
+		for (var i = 0; i < animalArray.length; i++) {
 
+					var arrayBtn = animalArray[i];
+					//adding iterative buttons to DOM
+					$('#animalButtons').append('<button class="animal">'+ arrayBtn);
+					// $('.animal').data("animal", arrayBtn);
+						
+				}
 
-//creating new button
-
+//adding input to array and creating new button=====================
 		$('#addAnimal').on('click', function() {
-			click++;
+			click = 1;
 			
-			total = total + click
-
+			total = (total - 1) + click;
+			
+			//getting value from input
 			var animal = $('#animalInput').val().trim();
 				console.log(animal);
 
+			//pushing to array
 			var addArray = animalArray.push(animal);
 				console.log(animalArray);
 
 			console.log(total);
-			//creating buttons from array
-			//Add new input to array add to button on screen
-				for (var i = 0; i < total; i++) {
-					var arrayBtn = animalArray[i]
-					console.log(arrayBtn);
-					$('#animalButtons').append('<button class="animal">'+ arrayBtn);
-					$('.animal').data("animal", arrayBtn);
-					
-				}
 
-			// var animalBtn = $('<button>').data("animal", animal).
-			// 				append(animal);
+			//creating buttons from array	
+			var arrayBtn = animalArray[total];
+			$('#animalButtons').append('<button class="animal">'+ arrayBtn);
+			$('.animal').data("animal", arrayBtn);
+			
 
 				return false;
 		});
 
 
-//query from button click
+//on button click trigger ajax query===============================
 $(document).on('click', '.animal', function() {
-			console.log("success :" + $(this).text());
         // var animalAttr = $(this).data('animal');
         var animalName = $(this).text().replace(" ", "+");
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
          animalName + "&api_key=dc6zaTOxFJmzC&limit=10";
          	console.log(queryURL);
 
+    //NOT WORKING     	
 	$.ajax({
-                url: queryURL,
-                method: 'GET'
+			url: queryURL, 
+			method: 'GET'
             })
-			.done( function(response) {
+		.done(function(response) {
 
 				var results = response.data;
 				console.log(results);
-
+		//iterate through all 10 objects called in ajax don't show r rating add rest to DOM
 		for(var i = 0; i < results.length; i++) {
 				if (results[i].rating == 'r') 
 				{
 
 				}
 				else{
-					var gifDiv = $('<div class = "animalItem">');
+					var gifDiv = $('<div class="animalItem">');
 					var rating = results[i].rating;
 					console.log(rating);
 
 					var p = $('<p>').text("Rating: " + rating);
-					var animalImage = $('<img class = "animalImage">')
+					var animalImage = $('<img class="animalImage">')
 									.attr('src', results[i].images.
 									fixed_height_still.url);
+									//.data('state', 'still');
 					console.log(animalImage);
 
 					$('#animals').append(gifDiv).append(p).append(animalImage);
 				} //else statement
 			}//for statement
-		});//ajax done
+		});//ajax done function
 	
 });//button click
 
+//add animate on click of gif
+$(document).on('click', '.animalImage', function() {
+
+	var state = $(this).attr('data-state');
+
+	
+
+	if ( state == 'still'){
+				var animate = $(this).attr('src');
+				animate.replace('_s', '');
+				console.log(animate);
+                $(this).attr('data-state', 'animate');
+            }else{
+                var animate = $(this).attr('src');
+				animate.replace('.', '_s.');
+				console.log(animate);
+                $(this).attr('data-state', 'still');
+            }
+		});
 
 //allow deletion of list 1-student-do-todolist
 
